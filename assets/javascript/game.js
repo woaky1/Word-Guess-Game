dbd60c36736e7d7e4e77aa0dd5894bc6f247c382
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-05 17:02:04 -0500

    did some more styling and page structure, then moved over to starting to code the game.

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// Establishing some variables.
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var guessesLeft = 15;
+var guessedLetters = [];
+var pokedex = [
+    {["p","i","k","a","c","h","u"]:["_","_","_","_","_","_","_"]}
+]
