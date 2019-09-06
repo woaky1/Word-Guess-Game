ddcf9f060cc3c25d624a4ab4f60e8c7b1d824dca
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-05 20:10:39 -0500

    got a blank to appear on the page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,5 +4,28 @@ var wins = 0;
 var guessesLeft = 15;
 var guessedLetters = [];
 var pokedex = [
-    {["p","i","k","a","c","h","u"]:["_","_","_","_","_","_","_"]}
-]
+            {name: ["p"], blanks: ["_"]}//,
+            // {name: ["p","i","k","a","c","h","u"], blanks: ["_","_","_","_","_","_","_"]}
+        ]
+var questionIndex = 0;
+
+function showQuestion() {
+    if (questionIndex <= (pokedex.length - 1)) {
+        document.querySelector("#currentWord").innerHTML = pokedex[questionIndex].blanks;
+    }
+}
+
+showQuestion();
+// This is what initiates the events of the game.
+document.onkeyup = function(event) {
+    // We record the users choice as a variable.
+    var userGuessInput = event.key;
+    var userGuess = userGuessInput.toLowerCase();
+    console.log(pokedex[questionIndex].blanks);
+    // Time to display the question
+
+
+    if (letters.includes(userGuess)) {
+
+    }
+}
