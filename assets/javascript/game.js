65614e6fb99799f2acd42d9f5312bb74e4641446
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-06 09:54:09 -0500

    Working on fresh approach.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,23 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var guessesLeft = 15;
 var guessedLetters = [];
-var pokedex = [
-            {name: ["p"], blanks: ["_"]}//,
-            // {name: ["p","i","k","a","c","h","u"], blanks: ["_","_","_","_","_","_","_"]}
-        ]
-var questionIndex = 0;
 
-function showQuestion() {
-    if (questionIndex <= (pokedex.length - 1)) {
-        document.querySelector("#currentWord").innerHTML = pokedex[questionIndex].blanks;
-    }
-}
+// We're linking parts of the html to variables here so we can diplay outcomes from the javascript on the page.
+var winsText = document.getElementById("wins-text");
+var guessesLeftText = document.getElementById("guesses-left-text");
+var lettersGuessedText = document.getElementById("guessed-letters-text");
+var currentWordText = document.getElementById("current-word");
 
-function replaceLetter () {
-        document.querySelector("#currentWord").innerHTML = pokedex[questionIndex].name;
-}
 // This is what initiates the events of the game.
 document.onkeyup = function(event) {
+
     // We record the users choice as a variable.
     var userGuessInput = event.key;
     var userGuess = userGuessInput.toLowerCase();
     console.log(userGuess);
-    console.log(pokedex[questionIndex].blanks);
-    console.log(pokedex[questionIndex].name);
-    // Time to display the question
-    console.log(letters.includes(userGuess));
-    console.log(userGuess === pokedex[questionIndex].name);
-    if (letters.includes(userGuess)) {
-        if (userGuess === pokedex[questionIndex].name) {
-            replaceLetter();
 
-        }
+    // Check if the key hit was a letter.
+    if (letters.includes(userGuess)) {
+        console.log("Yep, that's a letter.")
     }
 }
